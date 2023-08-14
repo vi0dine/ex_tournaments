1f7ff0c595f63edd79d237a2fe2dc6ec8a1b919c
Krzysztof Janiec <viodine@yahoo.com>
2023-08-14 11:59:50 +0200

    chore: :bookmark: bump version to 0.2.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExTournaments.MixProject do
   def project do
     [
       app: :ex_tournaments,
-      version: "0.2.3",
+      version: "0.2.4",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       description: description(),
