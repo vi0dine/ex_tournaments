523760b67ddf227d3afbfc98b607ba36b52d747e
Krzysztof Janiec <viodine@yahoo.com>
2023-08-21 16:00:32 +0200

    fix: :bug: adjust weights calculations to work for both swiss and 2lo

--- lib/ex_tournaments/pairings/swiss.ex
+++ lib/ex_tournaments/pairings/swiss.ex
@@ -6,11 +6,22 @@ defmodule ExTournaments.Pairings.Swiss do
   alias ExTournaments.Utils.EdmondsBlossom
   alias ExTournaments.Utils.EdmondsBlossom.Vertex
 
-  def generate_round(players, round, rated \\ false, colors \\ false) do
+  alias ExTournaments.Pairings.Swiss.Weight
+
+  def generate_round(
+        players,
+        round,
+        opts \\ [
+          rated: false,
+          colors: false,
+          bye_factor: 1.5,
+          up_down_factor: 1.2
+        ]
+      ) do
     players =
       players
-      |> assign_rating(rated)
-      |> assign_colors(colors)
+      |> assign_rating(opts)
+      |> assign_colors(opts)
       |> Enum.shuffle()
       |> Enum.with_index()
       |> Enum.map(fn {player, index} ->
@@ -38,7 +49,7 @@ defmodule ExTournaments.Pairings.Swiss do
     pairs =
       players
       |> assign_bye(score_pools)
-      |> build_pairs(rated, colors, score_pools, score_sums)
+      |> build_pairs(score_pools, score_sums, opts)
       |> Enum.sort_by(&{elem(&1, 0), elem(&1, 1)}, :asc)
       |> format_for_matching()
       |> EdmondsBlossom.call()
@@ -48,40 +59,43 @@ defmodule ExTournaments.Pairings.Swiss do
     generate_matches(round, players, pairs, bye_player)
   end
 
-  defp assign_rating(players, false), do: players
-
-  defp assign_rating(players, true) do
-    players
-    |> Enum.map(fn player ->
-      if is_nil(player.rating) do
-        %{player | rating: 0}
-      else
-        player
-      end
-    end)
+  defp assign_rating(players, opts) do
+    if Keyword.get(opts, :rated, false) do
+      players
+      |> Enum.map(fn player ->
+        if is_nil(player.rating) do
+          %{player | rating: 0}
+        else
+          player
+        end
+      end)
+    else
+      players
+    end
   end
 
-  defp assign_colors(players, false), do: players
-
-  defp assign_colors(players, true) do
-    players
-    |> Enum.map(fn player ->
-      if is_nil(player.colors) do
-        %{player | colors: []}
-      else
-        player
-      end
-    end)
+  defp assign_colors(players, opts) do
+    if Keyword.get(opts, :colors, false) do
+      players
+      |> Enum.map(fn player ->
+        if is_nil(player.colors) do
+          %{player | colors: []}
+        else
+          player
+        end
+      end)
+    else
+      players
+    end
   end
 
-  defp build_pairs(players, rated, colors, score_pools, score_sums) do
+  defp build_pairs(players, score_pools, score_sums, opts) do
     Enum.reduce(0..(length(players) - 1), [], fn player_index, acc ->
       current = Enum.at(players, player_index)
-
       next = Enum.slice(players, (player_index + 1)..-1)
 
-      sorted =
-        if rated do
+      sorted_players =
+        if Keyword.get(opts, :rated, false) do
           next
           |> Enum.sort(fn a, b ->
             abs(current.rating - a.rating) >= abs(current.rating - b.rating)
@@ -97,90 +111,20 @@ defmodule ExTournaments.Pairings.Swiss do
           score_sum_index =
             Enum.find_index(score_sums, fn s -> s == current.score + opponent.score end)
 
-          weight = 14 * :math.log10(score_sum_index + 1)
-
           score_group_index =
             Enum.find_index(score_pools, fn s -> s == current.score end) -
               Enum.find_index(score_pools, fn s -> s == opponent.score end)
 
-          score_group_diff = abs(score_group_index)
-
-          group_diff_factor =
-            if score_group_diff < 2,
-              do: 8 / :math.log10(score_group_diff + 2),
-              else: 1 / :math.log10(score_group_diff + 2)
-
-          weight = weight + group_diff_factor
-
           weight =
-            if score_group_diff == 1 and current.paired_up_down == false and
-                 opponent.paired_up_down == false do
-              weight + 1.2
-            else
-              weight
-            end
+            Weight.calculate_weight(current, opponent, score_sum_index, score_group_index,
+              rated: Keyword.get(opts, :rated, false),
+              sorted_players: sorted_players,
+              colors: Keyword.get(opts, :colors, false),
+              bye_factor: Keyword.get(opts, :bye_factor, 1.5),
+              up_down_factor: Keyword.get(opts, :up_down_factor, 1.2)
+            )
 
-          weight =
-            if rated do
-              weight +
-                (:math.log(length(sorted)) -
-                   :math.log(Enum.find_index(sorted, &(&1.id == opponent.id)) + 1)) / 3
-            else
-              weight
-            end
-
-          weight =
-            if colors do
-              color_score =
-                Enum.reduce(current.colors, 0, fn color, acc ->
-                  if color == "w" do
-                    acc + 1
-                  else
-                    acc - 1
-                  end
-                end)
-
-              opponent_score =
-                Enum.reduce(opponent.colors, 0, fn color, acc ->
-                  if color == "w" do
-                    acc + 1
-                  else
-                    acc - 1
-                  end
-                end)
-
-              cond do
-                length(current.colors) > 1 and Enum.take(current.colors, -2) == ["w", "w"] ->
-                  cond do
-                    Enum.join(Enum.take(opponent.colors, -2)) == "ww" -> weight
-                    Enum.join(Enum.take(opponent.colors, -2)) == "bb" -> weight + 7
-                    true -> weight + 2 / :math.log(4 - abs(opponent_score))
-                  end
-
-                length(current.colors) > 1 and Enum.take(current.colors, -2) == ["b", "b"] ->
-                  cond do
-                    Enum.join(Enum.take(opponent.colors, -2)) == "bb" -> weight
-                    Enum.join(Enum.take(opponent.colors, -2)) == "ww" -> weight + 8
-                    true -> weight + 2 / :math.log(4 - abs(opponent_score))
-                  end
-
-                true ->
-                  5 / (4 * :math.log10(6 - abs(color_score - opponent_score)))
-              end
-            else
-              weight
-            end
-
-          # TODO: Extract weight calculation to separate func/module and take factors from params
-
-          # weight =
-          #   if current.received_bye || opponent.received_bye do
-          #     weight * 1.5
-          #   else
-          #     weight
-          #   end
-
-          {:cont, acc ++ [{current.index, opponent.index, Float.round(weight / 100, 2)}]}
+          {:cont, acc ++ [{current.index, opponent.index, weight}]}
         end
       end)
     end)
